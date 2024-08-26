524d006b3a9fc5696c32334221b0e697d77ec7e2
muhycspb <dimka.k@bk.ru>
2024-08-26 19:44:35 +0300

    celery

---
+++ src/worker/insert_parcel_celery.py
@@ -0,0 +1,27 @@
+from celery import Celery
+from sqlalchemy import insert
+from src.database.database import engine
+from src.database.models import Parcel
+from src.routers.calculate_the_delivery import calculate
+
+
+rabbitmq_broker = Celery(broker="amqp://user:password@rabbitmq_container:5672/parcels")
+# rabbitmq_broker.conf.task_routes = {
+#     "insert_parcel": {"queue": "insert_parcel_queue"}
+# }
+
+
+@rabbitmq_broker.task
+async def insert_parcel(data, parcel_id, session_id):
+    """Добавление посылки в БД"""
+    async with engine.begin() as session:
+        parcel = insert(Parcel).values(
+            parcel_name=data["parcel_name"],
+            parcel_weight=data["parcel_weight"],
+            parcel_type=data["parcel_type"],
+            parcel_cost=data["parcel_cost"],
+            parcel_cost_delivery=await calculate(weight=data["parcel_weight"], cost=data["parcel_cost"]),
+            parcel_id=parcel_id,
+            parcel_session_id=session_id,
+        )
+        await session.execute(parcel)
